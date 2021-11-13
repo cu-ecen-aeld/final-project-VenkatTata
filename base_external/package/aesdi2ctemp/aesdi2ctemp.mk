6e44120543cbc8f9743cd6bdf952ab67f96dde90
Venkat Tata <saikrish1477@gmail.com>
2021-11-12 18:36:16 -0700

    i2c temperature sensor example

--- base_external/package/aesdi2ctemp/aesdi2ctemp.mk
+++ base_external/package/aesdi2ctemp/aesdi2ctemp.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference final project git contents
-AESDI2CTEMP_VERSION = '6275c50e974b05844806697f797a356c972df4dc'
+AESDI2CTEMP_VERSION = '4ce989e5ed27bcae7361e229ae29cb4befd50b3a'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
