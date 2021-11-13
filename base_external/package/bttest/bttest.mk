987f4ddc26821cc3da089b3d1cc4d713149f3ab1
Venkat Tata <saikrish1477@gmail.com>
2021-11-12 23:44:35 -0700

    bttest updated make file

--- base_external/package/bttest/bttest.mk
+++ base_external/package/bttest/bttest.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference final project git contents
-BTTEST_VERSION = '803b99155c51391cbf9645a015759afa1fc702d7'
+BTTEST_VERSION = '2d68b835ee645a38c2f3661b90c52ea61e8b29e2'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
